819a9565c927872a73a8c97373ba226e674a1e97
nettoandrade <antonio_netu@hotmail.com>
2020-05-28 23:16:56 -0300

    Primeiro Commit, Adição do Projeto base em Node.js

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,19 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res){
+    const UserExists = await User.findOne({ where: { email: req.body.email }});
+
+    if(UserExists){
+      return res.status(400).json({erro: 'Usuário já existe'});
+    }
+
+    const { id, name, email, provider} = await User.create(req.body);
+
+    return res.json({
+      id, name, email, provider
+    });
+  }
+}
+
+export default new UserController();
